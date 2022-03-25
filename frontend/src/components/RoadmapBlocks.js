d86ff2ed356b8e6ed004210d391032c7c1461f60
ken14250 <ken14250@gmail.com>
2022-03-25 15:58:24 +0800

    optimizated roadmap section

--- frontend/src/components/RoadmapBlocks.js
+++ frontend/src/components/RoadmapBlocks.js
@@ -4,8 +4,8 @@ function RoadmapBlocks({ title, desc }) {
   return (
     <div className="roadmap relative w-full sm:w-[32em] mx-auto flex flex-col justify-center space-y-10 sm:space-y-16 p-6 sm:p-8 z-10">
       <div className="flex flex-col space-y-4 justify-center">
-        <p className="sm:text-lg special-font">{title}</p>
-        <p className="">{desc}</p>
+        <p className="text-sm sm:text-lg special-font">{title}</p>
+        <p>{desc}</p>
       </div>
     </div>
   );
